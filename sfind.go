1bc42733caa27219436fbea44c040dfca9d57c5b
Damian Lee <damian@tag1consulting.com>
2016-03-09 10:50:56 +0000

    Add directory include and exclusivity flags, use helper functions

--- sfind.go
+++ sfind.go
@@ -29,64 +29,49 @@ func main() {
       Name: "insensitive, I",
       Usage: "Case insensitive matches",
     },
+    cli.BoolFlag {
+      Name: "include-dirs, d",
+      Usage: "Include directories in matches. This has presidence over 'dirs-only'",
+    },
+    cli.BoolFlag {
+      Name: "dirs-only, D",
+      Usage: "Only match directories",
+    },
   }
 
   app.Action = func(c *cli.Context) {
-    var base_path, pattern string
-    var err error
-    args := c.Args()
-    length := c.NArg();
+    base_path, pattern := determineArgs(c)
+    matcher := createMatcher(pattern, c)
+    result := createResult(c)
 
-    switch {
-      case length == 0:
-        fmt.Println("No arguments provided")
-        os.Exit(1)
-      case length == 1:
-        // Assume the single argument is a pattern. Default root to cwd.
-        base_path, err = os.Getwd()
-
-        if err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-        }
-
-        pattern = args.First()
-      case length > 1:
-        // Assume path will be the first arg, and pattern the second.
-        base_path = args.First()
-        pattern = args.Get(1)
-    }
-
-    var matcher FileMatcher
-    var result Result
-    invert := c.Bool("invert")
-    insensitive := c.Bool("insensitive")
-
-    if c.Bool("ext") {
-      matcher = newRegexMatcher(pattern, invert, insensitive)
-    } else {
-      matcher = FilepathMatcher{BaseMatcher{pattern, invert, insensitive}}
-    }
-
-    if c.Bool("count") {
-      result = &CountResult{}
-    } else {
-      result = &PrintResult{}
-    }
-
-    outputResults(base_path, result, matcher)
+    outputResults(base_path, result, matcher, c)
   }
 
   app.Run(os.Args)
 }
 
-func outputResults(base_path string, result Result, matcher FileMatcher) {
+func outputResults(base_path string, result Result, matcher FileMatcher, c *cli.Context) {
+  include_dirs := c.Bool("include-dirs")
+  dirs_only := c.Bool("dirs-only")
+  // Small shortcut to skip dir checks early.
+  no_dir_filters := !(include_dirs || dirs_only)
+
   // Allow the result to print something before files are walked.
   result.beforeResults()
 
   err := filepath.Walk(base_path, func (path string, fileInfo os.FileInfo, _ error) error {
-    // Skip dirs. Could make this configurable.
-    if fileInfo.IsDir() {
+    is_dir := fileInfo.IsDir();
+
+    // Determine how to deal with dirs based on any flags.
+    if (no_dir_filters && is_dir) {
+      // If there are no dir filters and this is dir, return. Otherwise, this
+      // will try to use the matcher below. By default, dirs are not included in
+      // results.
+      return nil
+    // This means there are dir filters, so check against them. Don't check
+    // include_dirs as if it hasn't been caught above (no_dir_filters) we want
+    // everything to go to below matching.
+    } else if (!is_dir && dirs_only) {
       return nil
     }
 
